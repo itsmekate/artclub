a6067c0144d97379094f1e1545fa62ca3bf769c9
NataliKukulevskaya <nataliia.ivanchenko@gmail.com>
2016-07-28 15:18:39 +0300

    add new project

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+//= ../../node_modules/bootstrap/js/carousel.js
+//= ../../node_modules/bootstrap/js/dropdown.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
