d0819184ef9ef15565a600ee70d6492fc2b14eef
NataliKukulevskaya <nataliia.ivanchenko@gmail.com>
2016-08-09 19:38:27 +0300

    check js build main

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,3 @@
-/*
- * Third party
- */
-//= ../../bower_components/jquery/dist/jquery.js
-//= ../../node_modules/bootstrap/dist/js/bootstrap.js
-
-//= ../../node_modules/bootstrap/js/carousel.js
-//= ../../node_modules/bootstrap/js/dropdown.js
-
-//= libs/responsiveslides.js
-//= libs/responsiveslides-call.js
-//= libs/jquery.maskedinput.min.js
-
-
-/*
- * Custom
- */
-
 $(document).ready(
     function() {
 //= partials/slider.js
